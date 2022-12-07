eeafa03fe2d119bb8318ad209036b956460d3558
x3dnesse <120049043+x3dnesse@users.noreply.github.com>
2022-12-07 12:17:05 -0700

    Changes to structuring of NETGEAR module

--- NETGEAR genie EN/setup.sh
+++ NETGEAR genie EN/setup.sh
@@ -1,9 +1,5 @@
 echo "Cleaning contents of /var/www/html before adding module"
-rm /var/www/html/index.html
-rm /var/www/html/received.php
-rm /var/www/html/.htaccess
-rm -r /var/www/html/css
-rm -r /var/www/html/img
+rm -r /var/www/html/*
 mkdir /var/www/html/css
 mkdir /var/www/html/img
 cp index.html /var/www/html/index.html
